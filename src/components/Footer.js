799816dd08c5f29481be5cc8808bedd96a8e2b79
Vendetta-Web <sk564228@gmail.com>
2023-09-06 16:11:34 +0530

    added detailin gooter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,19 +8,19 @@ export default function Footer() {
     <div className="container mx-auto flex justify-center items-center">
       <p>&copy; {currentYear} Vendetta-Web. All rights reserved.</p>
       <div className="ml-4">
-        <a href="https://trailhead.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.salesforce.com/trailblazer/saurabhdixit2303" target="_blank" rel="noopener noreferrer">
           <i className="fa-brands fa-salesforce text-gray-300 text-2xl mx-2 hover:text-blue-500"></i>
         </a>
         <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
           <i className="fa-brands fa-youtube text-gray-300 text-2xl mx-2 hover:text-red-700"></i>
         </a>
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/vendetta-web" target="_blank" rel="noopener noreferrer">
           <i className="fa-brands fa-github text-gray-300 text-2xl mx-2 hover:text-white"></i>
         </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://linkedin.com/saurabhdixit2303" target="_blank" rel="noopener noreferrer">
           <i className="fa-brands fa-linkedin text-gray-300 text-2xl mx-2 hover:text-blue-400"></i>
         </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+        <a href="https://instagram.com/imdixitsaurabh" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-instagram-square text-gray-300 text-2xl mx-2 hover:text-pink-500"></i>
         </a>
       </div>
