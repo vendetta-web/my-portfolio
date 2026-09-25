a4619bdabf84c5c31476b36c4af1188f4d2b2515
Saurabh Dixit <saurabh.dixit@siriusxm.com>
2026-09-25 17:00:57 +0530

    ok

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ import validator from "validator";
 export default function Contact() {
   const form = useRef();
   const [showSuccessAlert, setShowSuccessAlert] = useState(false);
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmitting] = useState(false);
   const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
   const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
   const userId = process.env.REACT_APP_EMAILJS_USER_ID;
